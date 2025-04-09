6478a8a1a9b01d1fdcd46c3632a007a58032f2a0
jonas-balb <jonas.balb@hotmail.com>
2025-04-09 20:40:18 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 basic.showIcon(IconNames.Heart)
 // Fahrbahnampel grün
-Environment.ledBrightness(AnalogPin.P13, true)
-Environment.ledBrightness(AnalogPin.P3, false)
-Environment.ledBrightness(AnalogPin.P4, false)
+Environment.ledBrightness(AnalogPin.P16, true)
+Environment.ledBrightness(AnalogPin.P3, true)
+Environment.ledBrightness(AnalogPin.P4, true)
 // Fußgängerampel rot
-Environment.ledBrightness(AnalogPin.P10, false)
-Environment.ledBrightness(AnalogPin.P7, false)
+Environment.ledBrightness(AnalogPin.P10, true)
+Environment.ledBrightness(AnalogPin.P15, true)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 0) {
         basic.showIcon(IconNames.Heart)
